2513fba72094eca8774985dd0eb32a2e6bdddd88
Daniil Timoschenko <carnolio@gmail.com>
2021-04-23 15:00:00 +0500

    Add files via upload

--- newsBot.py
+++ newsBot.py
@@ -11,46 +11,38 @@ import telebot, requests
 #import sqlite3
 
 
-botToken = "1700154841:AAEqEXDBhc4gZi02t4vttt6ZW5J6xKnYgPM"
-
-carnolioId = "124023217"
+botToken = ""
 categoryList = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology']
 
 botHelp = """
-    /addcat -   добавить категорию 
-    /addkey -   добавить ключ  
-    /delcat -   удалить категорию  
-    /delkey -   удалить ключ  
+    /addcat -   добавить категорию, можно несколько через пробел
+    /addkey -   добавить ключ, можно несколько через пробел  
+    /delcat -   удалить категорию, можно несколько через пробел  
+    /delkey -   удалить ключ, можно несколько через пробел  
     /getcat -   получить список ваших категорий
     /getcatlist -   получить список возможных категорий
     /getkey -   получить список ваших ключевых слов  
     /getnews -  получить 10 новостей в соответствии с набором 
                 категория/ключ 
     /help -     это сообщение
-    /start -    Зарегистрироваться, глянуть хелп если зарегистрирован
+    /start -    Зарегистрироваться
     """
 #pip3 install PyTelegramBotAPI
 #pip3 install newsapi-python
 
 bot = telebot.TeleBot(botToken, parse_mode = None)
-#bot = telebot.TeleBot(botToken)
 
-#def regNewUser(message):
-#    """ registration new user"""
-#    userId = message.from_user.id
-    #name = message.text
-    #msg="Пользователь "+name+" зарегистрирован"
-    #bot.send_message(message.from_user.id, msg, parse_mode=None)
 
 
 @bot.message_handler(commands=['start'])
 def startCommand(message):
     user_id = message.from_user.id
     data = {'user_id': user_id}
-    print(f"до сюда старт доходит, дальше запрос к серверу {user_id}")
+    # print(f"до сюда старт доходит, дальше запрос к серверу {user_id}")
     r = requests.post(url='HTTP://localhost:8080/users/', data=data)
-    print("че сервер вернул",r)
+    # print("че сервер вернул",r)
     bot.send_message(user_id, r.json(), parse_mode=None)
+    helpCommand(message)
 
 @bot.message_handler(commands=['help'])
 def helpCommand(message):
@@ -62,9 +54,9 @@ def getCatCommand(message):
     '''getcat'''
     user_id = message.from_user.id
     data = {'user_id': user_id}
-    r = requests.get(url='HTTP://localhost:8080/subscriptions/categories/', data=data)
+    r = requests.get(url='HTTP://localhost:8080/subscriptions/categories/', params=data)
     print(' '.join(r.json()))
-    bot.send_message(user_id, r.json(), parse_mode=None)
+    bot.send_message(user_id, '\n'.join(r.json()), parse_mode=None)
 
 @bot.message_handler(commands=['getkey'])
 def getKeyCommand(message):
@@ -72,27 +64,26 @@ def getKeyCommand(message):
     user_id = message.from_user.id
     data = {'user_id': user_id}
     r = requests.get(url='HTTP://localhost:8080/subscriptions/keywords/', params=data)
-    print(' '.join(r.json()))
-    bot.send_message(user_id, r.json(), parse_mode=None)
+    bot.send_message(user_id, '\n'.join(r.json()), parse_mode=None)
 
 @bot.message_handler(commands=['getcatlist'])
 def getCatListCommand(message):
     '''getcatlist'''
     bot.send_message(message.from_user.id, '\n '.join(categoryList), parse_mode=None)
 
-
-
-
 @bot.message_handler(commands=['addkey'])
 def addKeycommand(message):
     '''add keywords to user'''
     user_id = message.from_user.id
-    msg = message.text.split()
-    data = {'user_id': user_id, 'message': msg}
-    r = requests.post(url='HTTP://localhost:8080/subscriptions/keywords/', params=data)
-    print(' '.join(r.json()))
-    bot.send_message(user_id, r.json(), parse_mode=None)
-
+    msg = list(message.text.split())
+    print(msg)
+    msg = msg[1:]
+    if len(msg)==0:
+        bot.send_message(user_id, 'Пустая команда', parse_mode=None)
+    else:
+        data = {'user_id': user_id, 'message': msg}
+        r = requests.post(url='HTTP://localhost:8080/subscriptions/keywords/', data=data)
+        bot.send_message(user_id, '\n'.join(r.json()), parse_mode=None)
 
 @bot.message_handler(commands=['addcat'])
 def addCatCommand(message):
@@ -100,59 +91,63 @@ def addCatCommand(message):
     user_id = message.from_user.id
     msg = list(message.text.split())
     msg = msg[1:]
-    print("bot_add_cat",user_id,msg)
-    data = {'user_id': user_id, 'message': msg}
-    r = requests.post(url='HTTP://localhost:8080/subscriptions/categories/', params=data)
-    #print(' '.join(r.json()))
-    bot.send_message(user_id, r.json(), parse_mode=None)
+    if len(msg)==0:
+        bot.send_message(user_id, 'Пустая команда', parse_mode=None)
+    else:
+        print("bot_add_cat",user_id,msg)
+        data = {'user_id': user_id, 'message': msg}
+        r = requests.post(url='HTTP://localhost:8080/subscriptions/categories/', data=data)
+        #print(' '.join(r.json()))
+        bot.send_message(user_id, '\n'.join(r.json()), parse_mode=None)
 
 @bot.message_handler(commands=['delcat'])
 def delCatCommand(message):
     '''delcat'''
+    msg = list(message.text.split())
+    msg = msg[1:]
     user_id = message.from_user.id
-    msg = message.text.split()
-
-    data = {'user_id': user_id, 'message': msg}
-    r = requests.delete(url='HTTP://localhost:8080/subscriptions/categories/', params=data)
-    print(' '.join(r.json()))
-    bot.send_message(user_id, r.json(), parse_mode=None)
-
-
+    if len(msg)==0:
+        bot.send_message(user_id, 'Пустая команда', parse_mode=None)
+    else:
+        data = {'user_id': user_id, 'message': msg}
+        r = requests.delete(url='HTTP://localhost:8080/subscriptions/categories/', data=data)
+        bot.send_message(user_id, '\n'.join(r.json()), parse_mode=None)
 
 @bot.message_handler(commands=['delkey'])
 def delKeyCommand(message):
     '''delkey'''
     user_id = message.from_user.id
-    data = {'user_id': user_id, 'message': message.text}
-    r = requests.delete(url='HTTP://localhost:8080/subscriptions/keywords/', params=data)
-    print(' '.join(r.json()))
-    bot.send_message(user_id, r.json(), parse_mode=None)
-
-def regNewUser(message):
-    """ registration new user"""
-
+    msg = list(message.text.split())
+    msg = msg[1:]
+    if len(msg)==0:
+        bot.send_message(user_id, 'Пустая команда', parse_mode=None)
+    else:
+        data = {'user_id': user_id, 'message': msg}
+        r = requests.delete(url='HTTP://localhost:8080/subscriptions/keywords/', data=data)
+        bot.send_message(user_id, '\n'.join(r.json()), parse_mode=None)
 
 @bot.message_handler(commands=['getnews'])
 def getNewsCommand(message):
-    pass
+    '''get news'''
+    user_id = message.from_user.id
+    data = {'user_id': user_id}
+    r = requests.get(url='HTTP://localhost:8080/news/', params=data)
+    listNews=r.json()
+    print(listNews)
+    if listNews[0] == 'По такой выборке данных не найдено':
+        bot.send_message(user_id, f"По такой выборке данных не найдено", disable_web_page_preview=True)
+    else:
+        for i in range(len(listNews)):
+            title = listNews[i]["title"]
+            description = listNews[i]["description"]
+            url = listNews[i]["url"]
+            publishedAt = listNews[i]["publishedAt"]
+            #msg.append( f"{title}\n\n{description}\n{url}\n{publishedAt}")
+            bot.send_message(user_id, f"{title}\n\n{description}\n{url}\n{publishedAt}",disable_web_page_preview=True)
+    #print("print msgnews", msg)
 
 def check_server():
     pass
-
-"""
-
-Принимать параметры надо так
-if request.method == 'POST':
-        message = request.form.getlist('message')
-        user_id = request.form['user_id']
-
-а не так
-
-if request.method == 'POST':
-        user_id = request.args.get('user_id')
-        message = request.args.get('message')
-"""
-
 
 check_server()
 bot.polling()
--- server_flask.py
+++ server_flask.py
@@ -12,21 +12,12 @@ pip3 install flask
 /news/ для получения списка новостей.
 Взаимодействует с базой данных.
 Взаимодействует с API https://newsapi.org/.
-Для проверки текущего пользователя можно использовать его telegram(vk) id.
-PS. для получения/добавления/удаления используйте HTTP глаголы.
-
 """
 
-
 app = Flask(__name__)
-botToken = "1700154841:AAEqEXDBhc4gZi02t4vttt6ZW5J6xKnYgPM"
-newsApiKey = "7e40013ca7ea498589545453e4cea074"
-carnolioId = "124023217"
+
+newsApiKey = ""
 categoryList = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology']
-
-# message.from_user.id
-
-# print('\n '.join(keyForAdd))
 
 def userExist(user_id):
     try:
@@ -38,7 +29,7 @@ def userExist(user_id):
         sqlConn.commit()
         cursor.close()
         if len(rows) > 0:
-            #print(rows)
+            # print(rows)
             return True
         else:
             return False
@@ -50,98 +41,96 @@ def userExist(user_id):
 
 
 def add_cat(user_id, message):
-    catForAdd = message[1:]
     # print('\n '.join(catForAdd))
+    catForAdd = message
     msg = []
-    print(user_id, message)
+    # print(user_id, message)
     try:
         sqlConn = sqlite3.connect('newsBot.db')
-        for cat in range(len(catForAdd)):
-            #если категория из списка то добавляем
-            #print(catForAdd[cat])
-            if (catForAdd[cat] in categoryList) and (catExist(catForAdd[cat], user_id) == False):
-                sqlInsertCategory = f"INSERT INTO categories (name, user_id) VALUES ('{catForAdd[cat]}', '{user_id}');"
+        for cat in catForAdd:
+            # если категория из списка то добавляем
+            # print(catForAdd[cat])
+            if (cat in categoryList) and (catExist(cat, user_id) == False):
+                sqlInsertCategory = f"INSERT INTO categories (name, user_id) VALUES ('{cat}', '{user_id}');"
                 cursor = sqlConn.cursor()
                 cursor.execute(sqlInsertCategory)
                 sqlConn.commit()
                 cursor.close()
-                msg.append(catForAdd[cat] + " добавлена")
-            elif catExist(catForAdd[cat],user_id):
-                msg.append(catForAdd[cat] + " уже в вашем списке")
+                msg.append(cat + " добавлена")
+            elif catExist(cat, user_id):
+                msg.append(cat + " уже в вашем списке")
             else:
-                msg.append(catForAdd[cat] + " такой категории нет")
-
+                msg.append(cat + " такой категории нет")
     except sqlite3.Error as error:
         print("Ошибка при работе с SQLite", error)
+        msg = "Что-то пошло не так"
     finally:
         if sqlConn:
             sqlConn.close()
-    print("222 ", msg)
-    return msg
+        # print(msg)
+        return msg
 
 def del_cat(user_id, message):
-    catForDel = message.text.split()
-    #print('\n '.join(catForDel))
-
+    catForDel = message
+    # print('\n '.join(catForDel))
+    msg = list()
     try:
         sqlConn = sqlite3.connect('newsBot.db')
         sqlDelCategory = "DELETE FROM categories where name = ? and user_id = ?;"
-        for cat in range(1,len(catForDel)):
-            #print(catForDel[cat])
-            #if catForDel[cat] in categoryList and catForDel[cat] != "/delcat" and catExist(catForDel[cat],message.from_user.id) == True:
-            if catForDel[cat] in categoryList and catForDel[cat] != "/delcat" :
+        for cat in range(len(catForDel)):
+            # print(catForDel[cat])
+            if catForDel[cat] in categoryList and catExist(catForDel[cat],user_id) == True:
+                #print("jnjn")
                 cursor = sqlConn.cursor()
-                data_tuple = (catForDel[cat], message.from_user.id)
+                data_tuple = (catForDel[cat], user_id)
                 cursor.execute(sqlDelCategory, data_tuple)
                 sqlConn.commit()
                 cursor.close()
-                msg = catForDel[cat] + " удалена"
+                msg.append(catForDel[cat] + " удалена")
             else:
-                msg = catForDel[cat] + " такой категории нет"
-            #bot.send_message(message.from_user.id, msg, parse_mode=None)
-            return jsonify(msg)
+                msg.append(catForDel[cat] + " такой категории нет")
     except sqlite3.Error as error:
         print("Ошибка при работе с SQLite", error)
+        msg.append("Что-то пошло не так")
     finally:
         if sqlConn:
             sqlConn.close()
+    print(msg)
+    return msg
 
 
 
 def add_key(user_id, message):
-    keyForAdd = message.text.split()
+    keyForAdd = message
+    msg=list()
     try:
         sqlConn = sqlite3.connect('newsBot.db')
         sqlInsertKey = "INSERT INTO keywords (name, user_id) VALUES (?, ?);"
-        for key in range(1, len(keyForAdd)):
-            # print("пытаемся добавить", keyForAdd[key])
-
-            if keyForAdd[key] != "/addkey" and keyExist(keyForAdd[key], user_id) == False:
+        for key in range(len(keyForAdd)):
+            print("пытаемся добавить", keyForAdd[key])
+            if keyExist(keyForAdd[key], user_id) == False:
                 cursor = sqlConn.cursor()
                 data_tuple = (keyForAdd[key], user_id)
                 cursor.execute(sqlInsertKey, data_tuple)
                 sqlConn.commit()
                 cursor.close()
-                msg = keyForAdd[key] + "- ключ добавлен"
+                msg.append(keyForAdd[key] + "- ключ добавлен")
             else:
-                msg = "Такой ключ уже есть"
-            #bot.send_message(message.from_user.id, msg, parse_mode=None)
-            return jsonify(msg)
+                msg.append("Такой ключ уже есть")
     except sqlite3.Error as error:
-        print("Ошибка при работе с SQLite", error)
+        msg.append("Что-то пошло не так")
     finally:
         if sqlConn:
             sqlConn.close()
+        return msg
 
-
-def keyExist(key,user_id):
+def keyExist(key, user_id):
     rows = list()
     '''if key exist'''
     sqlConn = sqlite3.connect('newsBot.db')
     cursor = sqlConn.cursor()
     sqlSelectKeys = f"SELECT * FROM keywords WHERE user_id = '{user_id}' and name = '{key}'"
     cursor.execute(sqlSelectKeys)
-    #print(cursor.execute(sqlSelectKeys, data_tuple))
     rows = cursor.fetchall()
     sqlConn.commit()
     cursor.close()
@@ -150,9 +139,11 @@ def keyExist(key,user_id):
     else:
         return False
 
-def catExist(cat,user_id):
+
+def catExist(cat, user_id):
     rows = list()
-    '''if key exist'''
+    '''if cat exist'''
+    #print(cat, user_id)
     sqlConn = sqlite3.connect('newsBot.db')
     cursor = sqlConn.cursor()
     sqlSelectCats = f"SELECT name FROM categories WHERE user_id = '{user_id}' and name = '{cat}'"
@@ -164,6 +155,7 @@ def catExist(cat,user_id):
         return True
     else:
         return False
+
 
 def init_db():
     """Подключение к БД и создание таблиц"""
@@ -192,56 +184,51 @@ def init_db():
         if (sqlConn):
             sqlConn.close()
 
-def del_key(user_id,message):
-    #print('\n '.join(keyForDel))
-    keyForDel = message.text.split()
-
+def del_key(user_id, message):
+    msg=list()
+    keyForDel = message
     try:
         sqlConn = sqlite3.connect('newsBot.db')
-
-        for key in range(1,len(keyForDel)):
-            #if keyForDel[key] != "/delkey" and keyExist(keyForDel[key], message.from_user.id) == True:
-            if keyForDel[key] != "/delkey":
+        for key in range(len(keyForDel)):
+            if keyExist(keyForDel[key], user_id) == True:
                 sqlDelKey = f"DELETE FROM keywords WHERE name = '{keyForDel[key]}' AND user_id = '{user_id}'"
                 cursor = sqlConn.cursor()
-                #data_tuple = (keyForDel[key], message.from_user.id)
-                #cursor.execute(sqlDelKey, data_tuple)
                 cursor.execute(sqlDelKey)
                 sqlConn.commit()
                 cursor.close()
-                msg = keyForDel[key] + " ключ удален"
+                msg.append(keyForDel[key] + " ключ удален")
             else:
-                msg = keyForDel[key] + " Такого ключа нет"
-            #bot.send_message(message.from_user.id, msg, parse_mode=None)
-            return jsonify(msg)
+                msg.append(keyForDel[key] + " Такого ключа нет")
+
     except sqlite3.Error as error:
         print("Ошибка при работе с SQLite", error)
-        return jsonify(error)
+        msg.append("Что-то пошло не так")
     finally:
         if sqlConn:
             sqlConn.close()
-
+        return msg
 
 def add_user(user_id):
     try:
+        sqlConn = sqlite3.connect('newsBot.db')
         if userExist(user_id):
             msg = f"Пользователь {user_id} уже зарегистрирован"
-            print(f"Пользователь {user_id} уже зарегистрирован")
+            # print(f"Пользователь {user_id} уже зарегистрирован")
         else:
-            sqlConn = sqlite3.connect('newsBot.db')
             cursor = sqlConn.cursor()
             sqlInsertNewUser = f"INSERT INTO users (id) VALUES ('{user_id}');"
             cursor.execute(sqlInsertNewUser)
             sqlConn.commit()
             cursor.close()
             msg = f"Пользователь {user_id} зарегистрирован"
-            print(f"Пользователь {user_id} зарегистрирован")
-        return jsonify(msg)
+            # print(f"Пользователь {user_id} зарегистрирован")
+        return msg
     except sqlite3.Error as error:
         print("Ошибка при работе с SQLite", error)
+        msg = "Что-то пошло не так"
     finally:
-        if sqlConn:
-            sqlConn.close()
+       sqlConn.close()
+    return msg
 
 
 def get_key(user_id):
@@ -256,40 +243,44 @@ def get_key(user_id):
         sqlConn.commit()
         cursor.close()
     except sqlite3.Error as error:
-        print(error)
+        # print(error)
+        msg = "Что-то пошло не так"
+        return msg
     finally:
-        if sqlConn:
-            sqlConn.close()
-        if len(rows) > 0:
-            for item in rows:
-                listKeyword.append(item[0])
-            #if isPrint:
-            #    bot.send_message(message.from_user.id, '\n '.join(listKeyword), parse_mode=None)
-        else:
-            #bot.send_message(message.from_user.id, "Нет ключевых слов", parse_mode=None)
-            listKeyword.append("Нет ключевых слов")
-        return listKeyword
+       if sqlConn:
+           sqlConn.close()
+    if len(rows) > 0:
+        for item in rows:
+            listKeyword.append(item[0])
+    else:
+        listKeyword.append("Нет ключевых слов")
+    return listKeyword
 
 
-
-
-
-@app.route('/users/', methods=['GET','POST','DELETE'])
+@app.route('/users/', methods=['POST'])
 def users():
     """users"""
     if request.method == 'POST':
         user_id = request.form['user_id']
-        #print("/users",user_id)
+        # print("/users",user_id)
         return jsonify(add_user(user_id))
 
+
 def get_news(user_id):
-    listKeywords = get_key(user_id)#getKeyCommand(message,isPrint=False)
-    listCategory = get_cat(user_id)#(message,isPrint=False)
+    listKeywords = get_key(user_id)  # getKeyCommand(message,isPrint=False)
+    listCategory = get_cat(user_id)  # (message,isPrint=False
+    # print("lists_news",listKeywords,listCategory)
+    if listCategory[0] == 'Нет подписок на категории':
+        listCategory=[]
+    if listKeywords[0] == 'Нет ключевых слов':
+        listKeywords=[]
+
     listSources = []
     listNews = []
+    msg = []
 
     newsapi = NewsApiClient(api_key=newsApiKey)
-    #sources = newsapi.get_sources()
+    # sources = newsapi.get_sources()
     if len(listCategory) > 0:
         for category in listCategory:
             sources = newsapi.get_sources(category=category)
@@ -314,113 +305,88 @@ def get_news(user_id):
                 "url": response["articles"][i]["url"],
                 "publishedAt": response["articles"][i]["publishedAt"],
             })
-    #return listNews
+        msg = listNews
+        print("artic",msg)
     else:
-        #bot.send_message(message.from_user.id, "По такой выборке данных не найдено")
-        msg = "По такой выборке данных не найдено"
-        return jsonify(msg)
-
-    for i in range(len(listNews)):
-        title = listNews[i]["title"]
-        description = listNews[i]["description"]
-        url = listNews[i]["url"]
-        publishedAt = listNews[i]["publishedAt"]
-        msg = f"{title}\n\n{description}\n{url}\n{publishedAt}"
-        #bot.send_message(message.from_user.id, f"{title}\n\n{description}\n{url}\n{publishedAt}",disable_web_page_preview=True)
-        return jsonify(msg)
+        msg.append("По такой выборке данных не найдено")
+    return msg
 
 
 def get_cat(user_id):
     '''getcat'''
     userCats = list()
     rows = list()
+    print(user_id)
     try:
         sqlConn = sqlite3.connect('newsBot.db')
         cursor = sqlConn.cursor()
-        sqlSelectCats = f"SELECT name FROM categories WHERE user_id = '{user_id}'"
+        sqlSelectCats = f"SELECT name FROM categories WHERE user_id ='{user_id}'"
         cursor.execute(sqlSelectCats)
         rows = cursor.fetchall()
+        print(rows)
         sqlConn.commit()
         cursor.close()
     except sqlite3.Error as error:
-        print(error)
+        msg = "Что-то пошло не так"
     finally:
         if sqlConn:
             sqlConn.close()
-
         if len(rows) > 0:
-            #print(rows)
+            print(rows)
             for item in rows:
                 userCats.append(item[0])
-        #если ничего не пришло
+        # если ничего не пришло
         else:
             userCats.append("Нет подписок на категории")
         return userCats
 
 
-
-
-@app.route('/subscriptions/categories/', methods=['GET','POST','DELETE'])
+@app.route('/subscriptions/categories/', methods=['GET', 'POST', 'DELETE'])
 def subscriptions_categories():
     # add_cat
     if request.method == 'POST':
         message = request.form.getlist('message')
         user_id = request.form['user_id']
         return jsonify(add_cat(user_id, message))
-    #del_key
+    # del_key
     elif request.method == 'DELETE':
         message = request.form.getlist('message')
         user_id = request.form['user_id']
         return jsonify(del_cat(user_id, message))
-    #get_key
+    # get_key
     elif request.method == 'GET':
-        user_id= request.args.get('user_id')
+        user_id = request.args.get('user_id')
+        print("GET",user_id)
         return jsonify(get_cat(user_id))
 
 
-
-@app.route('/subscriptions/keywords/', methods=['GET','POST','DELETE'])
+@app.route('/subscriptions/keywords/', methods=['GET', 'POST', 'DELETE'])
 def subscriptions_keywords():
-    #add_key
+    # add_key
     if request.method == 'POST':
         message = request.form.getlist('message')
         user_id = request.form['user_id']
         return jsonify(add_key(user_id, message))
-    #del_key
+    # del_key
     elif request.method == 'DELETE':
-        user_id = request.args.get('user_id')
-        message = request.args.get('message')
+        message = request.form.getlist('message')
+        user_id = request.form['user_id']
         return jsonify(del_key(user_id, message))
-    #get_key
+    # get_key
     elif request.method == 'GET':
-        user_id= request.args.get('user_id')
+        user_id = request.args.get('user_id')
         return jsonify(get_key(user_id))
 
-@app.route('/news/', methods=['GET','POST','DELETE'])
+
+@app.route('/news/', methods=['GET'])
 def news():
-    pass
-
-
-"""
-@app.route('/rest', methods=['GET'])
-def rest():
-    user = {"name":"nick", 'email':'example.com', 'tel':['+790853535','+783294892384']}
-    return jsonify(user)
-
-@app.route('/calc', methods=['GET','POST'])
-def calc():
     if request.method == 'GET':
-        return render_template('calc.html')
-    if request.method == 'POST':
-        a = float(request.form.get('number_one'))
-        b = float(request.form.get('number_two'))
-        if request.form['calc'] == 'Сумма':
-            c = a + b
-        if request.form['calc'] == 'Разность':
-            c = a - b
-        return render_template('calc.html', c=c)
-"""
+        user_id = request.args.get('user_id')
+        #print('/news/',user_id)
+        return jsonify(get_news(user_id))
+
 
 if __name__ == '__main__':
     init_db()
+
     app.run(host='localhost', port=8080)
